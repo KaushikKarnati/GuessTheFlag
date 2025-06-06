d7aa0411a2708a12fe4965c031cd319dc9d16c9b
KaushikKarnati <kaushik.karnati@icloud.com>
2025-06-06 14:50:15 -0500

    Experimenting with gradients and buttons

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,14 +9,85 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        
+        
+        
+        
+        ZStack{
+            VStack(spacing: 0){
+                Color.red
+                Color.blue
+            }
+            
+            Text("Hello World!!")
+                .foregroundStyle(.secondary)
+                .padding(50)
+                .background(.ultraThinMaterial)
         }
-        .padding()
+        .ignoresSafeArea()
+        
+        Button("Delete",role: .destructive, action: executeDelete)
+        
+        VStack{
+            Button("Button 1"){}
+                .buttonStyle(.bordered)
+            Button("Button 2", role: .destructive){}
+                .buttonStyle(.bordered)
+            Button("Button 3"){}
+                .buttonStyle(.borderedProminent)
+                .tint(.mint)
+            Button("Button 4", role: .destructive){}
+                .buttonStyle(.borderedProminent)
+            
+            Button{
+                print("Button Pressed")} label: {
+                    Text("Tap Me")
+                        .padding()
+                        .foregroundStyle(.white)
+                        .background(.red)
+                    
+                }
+            
+            HStack{
+                Button{
+                    print("Button Pressed")
+                }label: {
+                    Image(systemName: "pencil")
+                        
+                }
+                
+                Button("Edit", systemImage: "pencil"){
+                    print("This was pressed")
+                }
+                
+                Button{
+                    print("Button Pressed")
+                }label: {
+                    Label("Edit",systemImage: "pencil")
+                        .padding()
+                        .foregroundStyle(.white)
+                        .background(.red)
+                        
+                }
+                
+            }
+            .padding()
+            
+            .background(.ultraThinMaterial)
+            
+            }
+        }
+        
+        
+       
+        
+      
     }
+
+
+func executeDelete(){
+    print ("Deleting message")
 }
 
 #Preview {
